d32bfbdc66169f0390f0cd2b47972ecc71e03364
mettxnc <matthis.rsx@gmail.com>
2024-10-24 04:58:08 +0200

    Button slider

--- assets/js/homeSlider.js
+++ assets/js/homeSlider.js
@@ -1,19 +1,26 @@
+let prec = 'recrutement';
 function ChangeSlide(slide){
+    document.getElementById(prec).style.backgroundColor = 'white';
+
     switch (slide) {
         case "recrutement":
             document.getElementById('slide').src = "assets/img/1.jpg";
-            document.getElementById('recrutement').style.backgroundColor = "red";
+            document.getElementById(slide).style.backgroundColor = "red";
             break;
         case "interim":
-            document.getElementsByClassName('test').styles.backgroundColor = 'white';
             document.getElementById('slide').src = "assets/img/2.jpg";
+            document.getElementById(slide).style.backgroundColor = "red";
             break;
         case "performance":
-            document.getElementById('slide').src = "assets/img/3.jpg";  // #bah_blud_ratio 
-            break;                                                      // dans un script JavaScript en plus
+            document.getElementById('slide').src = "assets/img/3.jpg";
+            document.getElementById('performance').style.backgroundColor = "red";
+            break;
         case "transformation":
             document.getElementById('slide').src = "assets/img/4.jpg";
-            
+            document.getElementById('transformation').style.backgroundColor = "red";
             break;
     }
+
+    prec = slide;
 }
+ 
