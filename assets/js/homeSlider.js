9fe4634c34e793587141626b2155409f57b08d24
mettxnc <matthis.rsx@gmail.com>
2024-12-12 11:41:29 +0100

    adding header over view

--- assets/js/homeSlider.js
+++ assets/js/homeSlider.js
@@ -1,26 +1,26 @@
-let prec = 'recrutement';
-function ChangeSlide(slide){
-    document.getElementById(prec).style.backgroundColor = 'white';
+let prec = 'recrutement'; // Le bouton actif initial
 
+function ChangeSlide(slide) {
+    // Réinitialise le bouton précédent
+    document.getElementById(prec).classList.remove('active'); 
+
+    // Change l'image affichée selon le bouton cliqué
     switch (slide) {
         case "recrutement":
             document.getElementById('slide').src = "assets/img/1.jpg";
-            document.getElementById(slide).style.backgroundColor = "red";
             break;
         case "interim":
             document.getElementById('slide').src = "assets/img/2.jpg";
-            document.getElementById(slide).style.backgroundColor = "red";
             break;
         case "performance":
             document.getElementById('slide').src = "assets/img/3.jpg";
-            document.getElementById('performance').style.backgroundColor = "red";
             break;
         case "transformation":
             document.getElementById('slide').src = "assets/img/4.jpg";
-            document.getElementById('transformation').style.backgroundColor = "red";
             break;
     }
 
-    prec = slide;
+    // Ajoute l'effet d'animation au nouveau bouton
+    document.getElementById(slide).classList.add('active'); 
+    prec = slide; // Met à jour le bouton actif
 }
- 
